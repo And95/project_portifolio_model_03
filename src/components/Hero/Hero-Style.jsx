18c8c1b37cd0fb20f38896f55b61e2b482bc5459
André Gonzaga <andregonzaga95@gmail.com>
2026-05-07 14:53:42 -0300

    fix: height bg-hero.png

--- src/components/Hero/Hero-Style.jsx
+++ src/components/Hero/Hero-Style.jsx
@@ -12,8 +12,8 @@ export const HeroContainer = styled.section`
   gap: ${pxToRem(32)};
 
   background-image: url(${heroBg});
-  background-size: cover;
-  background-position: center;
+  background-size: 100% auto;
+  background-position: top center;
   background-repeat: no-repeat;
 
   padding: ${pxToRem(120)} ${pxToRem(16)};
